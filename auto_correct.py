0c1b12f91c86122352d44bf7c1f6ef4cfb332556
Mohamadreza Kariminejad <77078797+mokar2001@users.noreply.github.com>
2022-01-14 19:51:10 +0330

    Completed

--- auto_correct.py
+++ auto_correct.py
@@ -6,6 +6,8 @@ from itertools import permutations
 import string
 from functools import lru_cache
 
+
+# Find all permutations by removing one letter
 def delete_letter(word):
     delete_l = set()
     for i in range(len(word)):
@@ -13,6 +15,8 @@ def delete_letter(word):
         delete_l.add(cur_word)
     return delete_l
 
+
+# Find all permutations by replacing one letter with a-z
 def replace_letter(word):
     replace_l = set()
     for i in range(len(word)):
@@ -21,6 +25,8 @@ def replace_letter(word):
             replace_l.add(cur_word +  c + word[i+1:])
     return replace_l
 
+
+# Find all permutations by inserting one letter between a-z
 def insert_letter(word):
     insert_l = set()
     for i in range(len(word)+1):
@@ -29,6 +35,8 @@ def insert_letter(word):
             insert_l.add(cur_word +  c + word[i:])
     return insert_l
 
+
+# Find all related word to our given word by apply one of the top functions
 def edit_one_letter(word):
     edit_one_set = set()
     edit_one_set.update(delete_letter(word))
@@ -36,6 +44,8 @@ def edit_one_letter(word):
     edit_one_set.update(insert_letter(word))
     return edit_one_set
 
+
+# Find all related word to our given word by apply twice of the top functions (nested)
 def edit_two_letters(word):
     edit_one_set = edit_one_letter(word)
     edit_two_set = set()
@@ -45,6 +55,8 @@ def edit_two_letters(word):
         edit_two_set.update(insert_letter(w))
     return edit_two_set   
 
+
+# The cost of converting `w1` to `w2` by caching Minium Edit Distance algorithm
 @lru_cache(None)
 def min_edit_dist(w1, w2, s1, s2):
     if s1 == 0:
@@ -55,16 +67,20 @@ def min_edit_dist(w1, w2, s1, s2):
     if w1[s1 - 1] == w2[s2 - 1]:
         return min_edit_dist(w1, w2, s1 - 1, s2 - 1)
     
-    replace_cost = 2 + min_edit_dist(w1, w2, s1 - 1, s2 - 1)
-    delete_cost = 1 + min_edit_dist(w1, w2, s1 - 1, s2)
-    insert_cost = 1 + min_edit_dist(w1, w2, s1, s2 - 1)
+    replace_cost = 2 + min_edit_dist(w1, w2, s1 - 1, s2 - 1) # Replace costs 2
+    delete_cost = 1 + min_edit_dist(w1, w2, s1 - 1, s2) # Delete costs 1
+    insert_cost = 1 + min_edit_dist(w1, w2, s1, s2 - 1) # Insert costs 1
     
     return min(replace_cost, delete_cost, insert_cost)
 
+
+# What is the propability `word` appears in shakespear dataset 
 def happen_prob(word, text, all_word):
     our_word = all_word.count(word)
     return our_word / len(all_word)
 
+
+# Find the most similar word in shakespear to our given word by Minium Edit Distance and Probability of Appearing
 def autoCorrect(word):
     with open('shakespeare.txt') as file:
         data = file.read()
